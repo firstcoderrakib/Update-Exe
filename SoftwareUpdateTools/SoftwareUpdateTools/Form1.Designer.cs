50cc19b919860b407d27ee9d31d9bae8a2998e15
Rakib Islam <rakib.shehab@gmail.com>
2020-01-07 21:19:28 +0600

    Last commit

---
+++ SoftwareUpdateTools/SoftwareUpdateTools/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SoftwareUpdateTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxApplicationPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Button();
+            this.TroyeeVatSolutionsUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(279, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Example of  Application path: \\\\192.168.1.4\\Share Folder";
+            // 
+            // textBoxApplicationPath
+            // 
+            this.textBoxApplicationPath.Location = new System.Drawing.Point(323, 88);
+            this.textBoxApplicationPath.Name = "textBoxApplicationPath";
+            this.textBoxApplicationPath.Size = new System.Drawing.Size(247, 20);
+            this.textBoxApplicationPath.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Application Path";
+            // 
+            // Close
+            // 
+            this.Close.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close.Location = new System.Drawing.Point(375, 317);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(105, 45);
+            this.Close.TabIndex = 11;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // TroyeeVatSolutionsUpdate
+            // 
+            this.TroyeeVatSolutionsUpdate.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TroyeeVatSolutionsUpdate.Location = new System.Drawing.Point(313, 146);
+            this.TroyeeVatSolutionsUpdate.Name = "TroyeeVatSolutionsUpdate";
+            this.TroyeeVatSolutionsUpdate.Size = new System.Drawing.Size(208, 62);
+            this.TroyeeVatSolutionsUpdate.TabIndex = 10;
+            this.TroyeeVatSolutionsUpdate.Text = "Troyee Vat Solutions Update";
+            this.TroyeeVatSolutionsUpdate.UseVisualStyleBackColor = true;
+            this.TroyeeVatSolutionsUpdate.Click += new System.EventHandler(this.TroyeeVatSolutionsUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxApplicationPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.TroyeeVatSolutionsUpdate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxApplicationPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button TroyeeVatSolutionsUpdate;
+    }
+}
+
---
+++ SoftwareUpdateTools/SoftwareUpdateTools/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftwareUpdateTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void TroyeeVatSolutionsUpdate_Click(object sender, EventArgs e)
+        {
+            if (textBoxApplicationPath.Text == "")
+            {
+                MessageBox.Show("Please Write the Path");
+            }
+            else
+            {
+                try
+                {
+                    SaveSatting();
+                    //var versionInfo = FileVersionInfo.GetVersionInfo(@"\\192.168.1.4\Share Folder\Land_Cant_Fund.exe");
+                    //string versionServer = versionInfo.ProductVersion;
+                    String currentDirectory = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+                    //MessageBox.Show("" + currentDirectory);
+                    /*Assembly assembly = Assembly.GetExecutingAssembly();
+                    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+                    string version1 = fileVersionInfo.ProductVersion;)*/
+
+                    //var versionInfoCurrentDirectory = FileVersionInfo.GetVersionInfo(currentDirectory + "\\Land_Cant_Fund.exe");
+                    //string versionCurrent = versionInfoCurrentDirectory.ProductVersion;
+                    //MessageBox.Show("Server V " + versionServer + " Current P V" + versionCurrent);
+
+                    var macAddr = (from nic in NetworkInterface.GetAllNetworkInterfaces()
+                                   where nic.OperationalStatus == OperationalStatus.Up
+                                   select nic.GetPhysicalAddress().ToString()
+                                        ).FirstOrDefault();
+
+                    string Ipaddress = localIPAddress();
+
+                    string name = System.Environment.MachineName;
+                    DateTime pcDate = DateTime.Now;
+
+                    //if (versionServer != versionCurrent)
+                    //{
+                    string sourceDirectory = textBoxApplicationPath.Text;
+                    //string sourceDirectory = @"\\192.168.1.4\Share Folder";
+                    //string sourceDirectory = @"\\3BL-9\Software\Practise";
+                    string targetDirectory = currentDirectory;
+                    Copy(sourceDirectory, targetDirectory);
+                    MessageBox.Show("Software has been Updated");
+                    getSatting();
+                    string lines = "Mac Address : " + macAddr + " \r\nIp Address: " + Ipaddress + "\r\nPc Name: " + name + "\r\nDate: " + pcDate + " ";
+
+                    // Write the string to a file.
+                    System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\VatUpdateInfo.txt");
+                    file.WriteLine(lines);
+
+                    file.Close();
+                    //saveInformation();
+                    //}
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void getSatting()
+        {
+            textBoxApplicationPath.Text = Properties.Settings.Default.PathName;
+        }
+
+        private void Copy(string sourceDirectory, string targetDirectory)
+        {
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+            DirectoryInfo diTarget = new DirectoryInfo(targetDirectory);
+
+            CopyAll(diSource, diTarget);
+        }
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            // Copy each file into the new directory.
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                Console.WriteLine(@"Copying {0}\{1}", target.FullName, fi.Name);
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            // Copy each subdirectory using recursion.
+            foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir =
+                    target.CreateSubdirectory(diSourceSubDir.Name);
+                CopyAll(diSourceSubDir, nextTargetSubDir);
+            }
+        }
+
+        private string localIPAddress()
+        {
+            IPHostEntry host;
+            string localIP = "";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ip in host.AddressList)
+            {
+                localIP = ip.ToString();
+
+                string[] temp = localIP.Split('.');
+
+                if (ip.AddressFamily == AddressFamily.InterNetwork && temp[0] == "192")
+                {
+                    break;
+                }
+                else
+                {
+                    localIP = null;
+                }
+            }
+
+            return localIP;
+        }
+
+        private void SaveSatting()
+        {
+            Properties.Settings.Default.PathName = textBoxApplicationPath.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
